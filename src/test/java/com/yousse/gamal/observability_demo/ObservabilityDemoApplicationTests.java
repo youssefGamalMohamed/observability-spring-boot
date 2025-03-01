0792d840b996d0164e31d7591ae3b86739d3e934
youssefGamalMohamed <YoussefGamalMohamedElsayed@gmail.com>
2025-03-01 21:18:58 +0200

    feat: Adding Test Main Class for the App

---
+++ src/test/java/com/yousse/gamal/observability_demo/ObservabilityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yousse.gamal.observability_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservabilityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
