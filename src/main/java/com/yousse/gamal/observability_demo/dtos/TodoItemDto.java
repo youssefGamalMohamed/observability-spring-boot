02b432385ccb4414b7b4a427021ad930258c4002
youssefGamalMohamed <YoussefGamalMohamedElsayed@gmail.com>
2025-03-01 21:22:05 +0200

    feat(dtos): Added Dto for TodoItemDto

---
+++ src/main/java/com/yousse/gamal/observability_demo/dtos/TodoItemDto.java
@@ -0,0 +1,13 @@
+package com.yousse.gamal.observability_demo.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+
+@Builder
+public record TodoItemDto(String id, 
+                        @NotBlank(message = "Title Must Not Be Empty or Null") String title, 
+                        @NotBlank(message = "Description Must Not Be Empty or Null") String description,
+                         boolean completed
+    ) {
+
+}
