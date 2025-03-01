20b2551c99e3c33a32253c399416cfa50c20f4bb
youssefGamalMohamed <YoussefGamalMohamedElsayed@gmail.com>
2025-03-02 00:28:16 +0200

    feat(Actions Into Span): Added Logging for What Happen Into Current Span

--- src/main/java/com/yousse/gamal/observability_demo/services/PostServiceImpl.java
+++ src/main/java/com/yousse/gamal/observability_demo/services/PostServiceImpl.java
@@ -8,6 +8,7 @@ import com.yousse.gamal.observability_demo.dtos.CommentDto;
 import com.yousse.gamal.observability_demo.dtos.PostDto;
 import com.yousse.gamal.observability_demo.integrations.JsonPlaceHolderRestClient;
 
+import brave.Tracer;
 import io.micrometer.observation.annotation.Observed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,7 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 public class PostServiceImpl implements PostServiceIfc {
 
     private final JsonPlaceHolderRestClient postServiceRestClient;
-    
+    private final Tracer tracer;
     @Override
     public List<PostDto> findAllPosts() {
         log.info("Fetching all posts");
@@ -35,8 +36,10 @@ public class PostServiceImpl implements PostServiceIfc {
     @Observed(name = "findPostComments", contextualName = "findPostComments")
     public List<CommentDto> findPostComments(Integer id) {
         log.info("Fetching post comments by post id: {}", id);
+        tracer.currentSpan().annotate("Fetching post comments by post id: " + id);
         PostDto post = postServiceRestClient.findPostById(id);
         log.info("Post: {}", post);
+        tracer.currentSpan().annotate("Fetching All Comments for Post with Id " + id);
         List<CommentDto> comments = postServiceRestClient.findPostComments(id);
         return comments;
     }
