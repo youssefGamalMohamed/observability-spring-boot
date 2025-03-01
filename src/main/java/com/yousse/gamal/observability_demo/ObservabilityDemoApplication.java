c46bf05983aa35d792d3dc0854ea38bf2c38fcb8
youssefGamalMohamed <YoussefGamalMohamedElsayed@gmail.com>
2025-03-01 21:18:45 +0200

    feat: Adding Main Class of App

---
+++ src/main/java/com/yousse/gamal/observability_demo/ObservabilityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yousse.gamal.observability_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservabilityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservabilityDemoApplication.class, args);
+	}
+
+}
