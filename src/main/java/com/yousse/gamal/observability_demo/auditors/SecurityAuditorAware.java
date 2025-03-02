a7d35e48f34ab61d1bbde9d9eafdf1dd09901b14
youssefGamalMohamed <YoussefGamalMohamedElsayed@gmail.com>
2025-03-02 03:08:55 +0200

    feat(Auditor): Adding Auditor for any Entity from User Token

---
+++ src/main/java/com/yousse/gamal/observability_demo/auditors/SecurityAuditorAware.java
@@ -0,0 +1,24 @@
+package com.yousse.gamal.observability_demo.auditors;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.security.core.Authentication;
+
+@AllArgsConstructor
+public class SecurityAuditorAware implements AuditorAware<String> {
+    
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.of("UNKNOWN");
+        }
+        return Optional.of(authentication.getName());
+    }
+
+}
